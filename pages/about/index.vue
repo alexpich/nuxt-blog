5a71e7d59e3bf10e8a37b580ad96475831609f2a
alexpich <pich.alex01@gmail.com>
2020-05-17 00:53:00 -0700

    First commit

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>This is my About page!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia vel in, eveniet molestiae at ratione accusantium esse quo voluptates libero.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia vel in, eveniet molestiae at ratione accusantium esse quo voluptates libero.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia vel in, eveniet molestiae at ratione accusantium esse quo voluptates libero.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia vel in, eveniet molestiae at ratione accusantium esse quo voluptates libero.</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
