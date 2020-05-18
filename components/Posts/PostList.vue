5f0df560c7004c6321a5340da7b0315fc1ecd92f
alexpich <pich.alex01@gmail.com>
2020-05-18 03:51:17 -0700

    Check if isAdmin

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
       title="Hello there"
       previewText="First post"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
       title="Hello there"
       previewText="Second post"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
       title="Hello there"
       previewText="Third post"
@@ -26,6 +29,12 @@ import PostPreview from "@/components/Posts/PostPreview";
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 };
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Alex Pich",
+        title: "First Post",
+        content: "This is my content of my first post. Boring right?",
+        thumbnailLink:
+          "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
