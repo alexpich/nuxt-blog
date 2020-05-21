5692c97fff618496de6908be933615088bc5cf67
alexpich <pich.alex01@gmail.com>
2020-05-20 18:28:11 -0700

    Async data

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -12,7 +12,35 @@ import PostList from "@/components/Posts/PostList";
 export default {
   components: {
     PostList
-  }
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "first post",
+            previewText: "This my first posty post.",
+            thumbnail:
+              "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+          },
+          {
+            id: "2",
+            title: "second post",
+            previewText: "This my second posty post.",
+            thumbnail:
+              "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+          }
+        ]
+      });
+    }, 1500);
+  },
+  // data() {
+  //   return {
+  //     loadedPosts: []
+  //   };
+  // },
+  created() {}
 };
 </script>
 
